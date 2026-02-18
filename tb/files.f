1bc5079616ec10c39fe0fcb4ac9e74abedd27448
Kei Awano <awano.kei.kyoto@gmail.com>
2026-02-18 12:22:34 +0900

    feat: create files.f

---
+++ tb/files.f
@@ -0,0 +1,16 @@
+../single_cycle_processor/risc_v_32_i_pkg.sv
+
+../single_cycle_processor/ArithmeticLogicUnit.sv  
+../single_cycle_processor/ComparatorUnit.sv  
+../single_cycle_processor/CoreControlUnit.sv  
+../single_cycle_processor/ImmediateSignExtend.sv  
+../single_cycle_processor/InstructionMemory.sv  
+../single_cycle_processor/LoadStoreUnit.sv  
+../single_cycle_processor/ProcessorCore.sv  
+../single_cycle_processor/RegisterFile.sv  
+
+./DataMemory.sv  
+./InstructionMemory.sv  
+./top.sv 
+
+./tb_top.sv
